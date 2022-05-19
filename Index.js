a694bbc60fa3b28a7d06b2715ce3f93541f0be7e
Luis Arista <larista93@hotmail.com>
2022-05-19 18:06:40 -0500

    agregando la segunda grafica

--- Index.js
+++ Index.js
@@ -1,18 +1,25 @@
 import grafica from './grafica.js'
 
 
-const renderizar = (pokemon , imagenFrente , imagenShiny) => {
+const renderizar = (pokemon, tipo , imagenFrente , imagenShiny) => {
 
     let lugarImagen = document.getElementById('contenedor_imagenes')
     let lugarGraficas= document.getElementById('contenedor_graficas')
     
     lugarImagen.innerHTML = `
-    <div class="contenedor_pokeNombre">
-        <h2 class="pokeNombre">Informacion de : ${pokemon.toUpperCase()}</h2>
+    <div class="contenedor_pokeInfo">
+        <h2 class="pokeNombre">Nombre : ${pokemon.toUpperCase()}</h2>
+        <h2 class="pokeNombre">Tipo : ${tipo.toUpperCase()}</h2>
     </div>
     <div class="contenedor_pokeImagen">
-        <img src="${imagenFrente}" alt="" class="pokeImagen">
-        <img src="${imagenShiny}" alt="" class="pokeImagen">
+        <div >
+             <h4>Normal</h4>
+             <img src="${imagenFrente}" alt="" class="pokeImagen">
+        </div>
+        <div>
+             <h4>Shiny</h4>
+             <img src="${imagenShiny}" alt="" class="pokeImagen">
+        </div>
     </div>
     `
 }
@@ -29,12 +36,25 @@ const consultar = async () => {
    let imagenFrente = respuesta.sprites.front_default
    let imagenShiny = respuesta.sprites.front_shiny
    let tipo = respuesta.types[0].type.name
-   renderizar( pokemon , imagenFrente , imagenShiny)
+   let id = respuesta.id
+   let urlLocation = `https://pokeapi.co/api/v2/pokemon/${id}/encounters`
+   let ApiLocation = await fetch(urlLocation)
+   let respuestaLocation = await ApiLocation.json()
    
-   let canva = document.getElementById('grafica-Uno')
-    canva.innerHTML=''
+   console.log(respuesta)
+
+
+   renderizar( pokemon, tipo , imagenFrente , imagenShiny)
    
-   grafica(valorGrafica , pokemon , canva , tipo)
+   let canvaGraficaUno = document.getElementById('grafica-Uno')
+   let canvaGraficados = document.getElementById('grafica-Dos')
+    
+    
+   
+   grafica(valorGrafica , `Estadisticas de: ${pokemon.toUpperCase()}` , canvaGraficaUno , tipo, 'poder', 'uno' , 'radar' )
+   grafica(respuestaLocation , 'Region en la que se encuentra' , canvaGraficados , tipo, 'locacion', 'dos', 'bar' )
+
+  
 }
 
 let boton = document.getElementById('boton')
