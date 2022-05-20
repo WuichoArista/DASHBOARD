05b77cf9457cac74fe3fe939fcc3988170087e34
Luis Arista <larista93@hotmail.com>
2022-05-20 13:17:17 -0500

    agregando alerta

--- Index.js
+++ Index.js
@@ -4,7 +4,6 @@ import grafica from './grafica.js'
 const renderizar = (pokemon, tipo , imagenFrente , imagenShiny) => {
 
     let lugarImagen = document.getElementById('contenedor_imagenes')
-    let lugarGraficas= document.getElementById('contenedor_graficas')
     
     lugarImagen.innerHTML = `
     <div class="contenedor_pokeInfo">
@@ -29,33 +28,46 @@ const renderizar = (pokemon, tipo , imagenFrente , imagenShiny) => {
 const consultar = async () => {
    let buscador = document.getElementById('buscador').value
    let pokemon = buscador.toLowerCase()
-   let url = `https://pokeapi.co/api/v2/pokemon/${pokemon}`
-   let Api = await fetch(url)
-   let respuesta = await Api.json()
-   let valorGrafica = respuesta.stats
-   let imagenFrente = respuesta.sprites.front_default
-   let imagenShiny = respuesta.sprites.front_shiny
-   let tipo = respuesta.types[0].type.name
-   let id = respuesta.id
-   let urlLocation = `https://pokeapi.co/api/v2/pokemon/${id}/encounters`
-   let ApiLocation = await fetch(urlLocation)
-   let respuestaLocation = await ApiLocation.json()
-   
-   console.log(respuesta)
 
+   try{
 
-   renderizar( pokemon, tipo , imagenFrente , imagenShiny)
-   
-   let canvaGraficaUno = document.getElementById('grafica-Uno')
-   let canvaGraficados = document.getElementById('grafica-Dos')
-    
-    
-   
-   grafica(valorGrafica , `Estadisticas de: ${pokemon.toUpperCase()}` , canvaGraficaUno , tipo, 'poder', 'uno' , 'radar' )
-   grafica(respuestaLocation , 'Region en la que se encuentra' , canvaGraficados , tipo, 'locacion', 'dos', 'bar' )
+        let url = `https://pokeapi.co/api/v2/pokemon/${pokemon === '' ? pokemon = 'bulbasaur': pokemon }`
+        let Api = await fetch(url)
+        let respuesta = await Api.json()
+        let valorGrafica = respuesta.stats
+        let imagenFrente = respuesta.sprites.front_default
+        let imagenShiny = respuesta.sprites.front_shiny
+        let tipo = respuesta.types[0].type.name
+        let id = respuesta.id
+        let urlLocation = `https://pokeapi.co/api/v2/pokemon/${id}/encounters`
+        let ApiLocation = await fetch(urlLocation)
+        let respuestaLocation = await ApiLocation.json()
+        renderizar( pokemon, tipo , imagenFrente , imagenShiny)
+        let canvaGraficaUno = document.getElementById('grafica-Uno')
+        let canvaGraficados = document.getElementById('grafica-Dos')
+        grafica(valorGrafica , `Estadisticas de: ${pokemon.toUpperCase()}` , canvaGraficaUno , tipo, 'poder', 'uno' , 'radar' )
+        grafica(respuestaLocation , 'Region en la que se encuentra' , canvaGraficados , tipo, 'locacion', 'dos', 'bar' )
 
+   } catch(e){
+      
+        let alerta = document.getElementById('mensaje_alerta')
+        let lugarMensaje = document.getElementById('alerta_mensaje')
+        alerta.classList.add('mensaje_alerta_mostrar')
+        lugarMensaje.innerHTML = `
+        <p>El pokemon <b>${pokemon}</b> no fue encontrado, por favor revise si esta escrito correctamente</p>
+        `
+
+   }
   
 }
 
 let boton = document.getElementById('boton')
-boton.addEventListener( 'click' , consultar)
+    boton.addEventListener( 'click' , consultar)
+
+let botonAlerta = document.getElementById('boton_alert')
+    botonAlerta.addEventListener ( 'click' , () => {
+        let alerta = document.getElementById('mensaje_alerta')
+        alerta.classList.remove('mensaje_alerta_mostrar')
+    } )
+
+consultar()
